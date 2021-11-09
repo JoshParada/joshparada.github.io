dc8618451932cb7199a4c07708aad7a5da2581c9
Josh Parada <joshparada@Joshs-MacBook-Pro.local>
2021-11-09 14:55:10 -0600

    added files

--- main.js
+++ main.js
@@ -8,3 +8,15 @@ window.addEventListener('DOMContentLoaded', (e) => {
         })
     });
 })
+
+
+    function bigImg(x) {
+      x.style.height = "40%";
+      x.style.width = "40%";
+    }
+    
+    function normalImg(x) {
+      x.style.height = "32%";
+      x.style.width = "32%";
+    }
+    
